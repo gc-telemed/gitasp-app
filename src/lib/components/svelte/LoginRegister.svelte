80075cb22d604622f6f07c39bd9525165bcf8924
Risav Karna <git@risav.dev>
2024-08-06 20:48:45 +0545

    fix(ui): welcome page with oauth login options

--- src/lib/components/svelte/LoginRegister.svelte
+++ src/lib/components/svelte/LoginRegister.svelte
@@ -1,52 +1,49 @@
 <script lang="ts">
-let responseMessage: string;
+  import OAuthIn from "./OAuthIn.svelte";
+  import OAuthUp from "./OAuthUp.svelte";
 
-async function signIn(e: SubmitEvent) {
-  e.preventDefault();
-  try {
-    const response = await fetch("/api/sign-in", {
-      method: "POST",
-      body: new FormData(e.currentTarget as HTMLFormElement),
-    });
-    if (!response.ok) {
-      const errorData = await response.json();
-      responseMessage = errorData.message || "Sign in failed";
-    } else {
-      const data = await response.json();
-      responseMessage = data.message || "Sign in successful";
+
+  let responseMessage: string;
+
+  async function signInAction(e: SubmitEvent) {
+    const formData = new FormData(e.currentTarget as HTMLFormElement);
+    try {
+      // await signIn("credentials", {
+      //   username: formData.get("username"),
+      //   password: formData.get("password"),
+      //   redirect: false,
+      // });
+      window.location.href = "/cliniq";
+    } catch (error) {
+      console.error("Error during sign in:", error);
+      responseMessage = "An unexpected error occurred";
     }
-  } catch (error) {
-    console.error("Error during sign in:", error);
-    responseMessage = "An unexpected error occurred";
   }
-}
 
-
-async function signUp(e: SubmitEvent) {
-  e.preventDefault();
-  const formData = new FormData(e.currentTarget as HTMLFormElement);
-  if (formData.get("password") !== formData.get("passwordVerify")) {
-    responseMessage = "Passwords do not match.";
-    return;
-  }
-  try {
-    const response = await fetch("/api/sign-up", {
-      method: "POST",
-      body: formData,
-    });
-    if (!response.ok) {
-      const errorData = await response.json();
-      responseMessage = errorData.message || "Sign up failed";
-    } else {
-      const data = await response.json();
-      responseMessage = data.message || "Sign up successful";
+  async function signUp(e: SubmitEvent) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget as HTMLFormElement);
+    if (formData.get("password") !== formData.get("passwordVerify")) {
+      responseMessage = "Passwords do not match.";
+      return;
     }
-  } catch (error) {
-    console.error("Error during sign up:", error);
-    responseMessage = "An unexpected error occurred";
+    try {
+      const response = await fetch("/api/sign-up", {
+        method: "POST",
+        body: formData,
+      });
+      if (!response.ok) {
+        const errorData = await response.json();
+        responseMessage = errorData.message || "Sign up failed";
+      } else {
+        const data = await response.json();
+        responseMessage = data.message || "Sign up successful";
+      }
+    } catch (error) {
+      console.error("Error during sign up:", error);
+      responseMessage = "An unexpected error occurred";
+    }
   }
-}
-
 </script>
 
 <div class="border-b border-gray-200 dark:border-neutral-700">
@@ -88,59 +85,7 @@ async function signUp(e: SubmitEvent) {
     role="tabpanel"
     aria-labelledby="horizontal-alignment-item-1"
   >
-    <form on:submit={signIn}>
-      <div class="p-4 overflow-y-auto">
-        <label
-          for="input-user"
-          class="block float-left text-md font-medium mb-2 dark:text-white"
-          >Email/Username</label
-        >
-        <input
-          type="text"
-          id="input-user"
-          name="username"
-          class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-primary focus:ring-primary dark:bg-neutral-900 dark:border-neutral-700 dark:placeholder-neutral-500 dark:text-neutral-400"
-          placeholder="you@example.com"
-          autocomplete="username"
-          autofocus
-        />
-      </div>
-      <div class="pb-2 px-4 overflow-y-auto">
-        <label
-          for="input-pass"
-          class="block float-left text-md font-medium mb-2 dark:text-white"
-          >Password</label
-        >
-        <input
-          type="password"
-          id="input-pass"
-          name="password"
-          autocomplete="current-password"
-          class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-primary focus:ring-primary dark:bg-neutral-900 dark:border-neutral-700 dark:placeholder-neutral-500 dark:text-neutral-400"
-          placeholder="********"
-        />
-      </div>
-      <div
-        class="flex justify-between items-center gap-x-2 py-3 px-4 border-t dark:border-neutral-700"
-      >
-        <button
-          type="reset"
-          class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
-          data-hs-overlay="#hs-focus-management-modal"
-        >
-          Cancel
-        </button>
-        {#if responseMessage}
-        <p class="text-orange-500">{responseMessage}</p>
-        {/if}
-        <button
-          type="submit"
-          class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-primary text-white hover:bg-emerald-700 focus:outline-none focus:bg-emerald-700 disabled:opacity-50 disabled:pointer-events-none"
-        >
-          Sign In
-        </button>
-      </div>
-    </form>
+    <OAuthIn/>
   </div>
 
   <!-- sign up -->
@@ -150,73 +95,6 @@ async function signUp(e: SubmitEvent) {
     role="tabpanel"
     aria-labelledby="horizontal-alignment-item-2"
   >
-    <form on:submit={signUp}>
-      <div class="p-4 overflow-y-auto">
-        <label
-          for="input-username"
-          class="block float-left text-md font-medium mb-2 dark:text-white"
-          >Email/Username</label
-        >
-        <input
-          type="text"
-          id="input-username"
-          name="username"
-          autocomplete="username"
-          class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-primary focus:ring-primary dark:bg-neutral-900 dark:border-neutral-700 dark:placeholder-neutral-500 dark:text-neutral-400"
-          placeholder="you@example.com"
-          autofocus
-        />
-      </div>
-      <div class="pb-2 px-4 overflow-y-auto">
-        <label
-          for="input-password"
-          class="block float-left text-md font-medium mb-2 dark:text-white"
-          >Password</label
-        >
-        <input
-          type="password"
-          id="input-password"
-          name="password"
-          autocomplete="new-password"
-          class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-primary focus:ring-primary dark:bg-neutral-900 dark:border-neutral-700 dark:placeholder-neutral-500 dark:text-neutral-400"
-          placeholder="********"
-        />
-      </div>
-      <div class="pb-2 px-4 overflow-y-auto">
-        <label
-          for="input-pass2"
-          class="block float-left text-md font-medium mb-2 dark:text-white"
-          >Verify Password</label
-        >
-        <input
-          type="password"
-          id="input-pass2"
-          name="passwordVerify"
-          autocomplete="new-password"
-          class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-primary focus:ring-primary dark:bg-neutral-900 dark:border-neutral-700 dark:placeholder-neutral-500 dark:text-neutral-400"
-          placeholder="********"
-        />
-      </div>
-      <div
-        class="flex justify-between items-center gap-x-2 py-3 px-4 border-t dark:border-neutral-700"
-      >
-        <button
-          type="reset"
-          class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
-          data-hs-overlay="#hs-focus-management-modal"
-        >
-          Cancel
-        </button>
-        {#if responseMessage}
-        <p class="text-orange-500">{responseMessage}</p>
-        {/if}
-        <button
-          type="submit"
-          class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-primary text-white hover:bg-emerald-700 focus:outline-none focus:bg-emerald-700 disabled:opacity-50 disabled:pointer-events-none"
-        >
-          Sign Up
-        </button>
-      </div>
-    </form>
+    <OAuthUp/>
   </div>
 </div>
--- src/lib/db/auth.ts
+++ src/lib/db/auth.ts
@@ -1,3 +1,24 @@
-import { PrismaClient } from "@prisma/client";
 
-export const client = new PrismaClient();
+import { PrismaAdapter } from "@lucia-auth/adapter-prisma"
+import { PrismaClient } from "@prisma/client"
+import { Lucia } from "lucia";
+
+export const prisma = new PrismaClient()
+
+export const adapter = new PrismaAdapter(prisma.session, prisma.user);
+
+export const lucia = new Lucia(adapter, {
+	sessionCookie: {
+		attributes: {
+			// set to `true` when using HTTPS
+			secure: import.meta.env.PROD
+		}
+	}
+});
+
+
+declare module "lucia" {
+	interface Register {
+		Lucia: typeof lucia;
+	}
+}
